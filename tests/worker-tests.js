9d41139cc67b52bb94c4f2a8a7c93564df170f3f
daniel-lundin <daniel.t.lundin@gmail.com>
2016-12-18 22:56:56 +0100

    Naive process pool implementation

--- tests/worker-tests.js
+++ tests/worker-tests.js
@@ -6,20 +6,23 @@ const test = require('../lib/kuta.js').test;
 const runner = require('../lib/runner');
 
 test.before(() => {
-  console.log('before');
   sinon.spy(childProcess, 'fork');
 });
 
 test.after(() => {
-  console.log('after');
   childProcess.fork.restore();
 });
 
 test('should spawn processes for process pool', () => {
   const requires = [];
   const processCount = 2;
-  return runner.run(['test-files/passing-tests.js'], requires, processCount)
-    .then((results) => {
-      assert.equal(childProcess.fork.callCount, 2, "Should spawn two processes");
+  const files = [
+    'test-files/passing-tests.js',
+    'test-files/passing-tests.js',
+    'test-files/passing-tests.js'
+  ];
+  return runner.run(files, requires, processCount)
+    .then(() => {
+      assert.equal(childProcess.fork.callCount, 2, 'Should spawn two processes');
     });
 });
