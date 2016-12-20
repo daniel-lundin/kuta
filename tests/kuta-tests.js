b7ae545d2c82db2738636b8472c2d430c5472779
daniel-lundin <daniel.t.lundin@gmail.com>
2016-12-20 22:42:03 +0100

    Implemented test.group to be able to group after/before around a group of test

--- tests/kuta-tests.js
+++ tests/kuta-tests.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const sinon = require('sinon');
 
 const kuta = require('../lib/kuta');
 const common = require('../lib/common');
@@ -9,4 +10,27 @@ kuta.test('returning rejected promises should fail', () => {
     .then((result) => {
       assert.equal(result.result, common.TEST_FAILTURE, 'should fail');
     });
+});
+
+kuta.test.group('group test', (it) => {
+  const firstBeforeSpy = sinon.stub();
+  const firstAfterSpy = sinon.stub();
+  const secondBeforeSpy = sinon.stub();
+  const secondAfterSpy = sinon.stub();
+
+  it.group('first group', (t) => {
+    t.before(firstBeforeSpy);
+    t.after(firstAfterSpy);
+  });
+
+  it.group('second group', (t) => {
+    t.before(secondBeforeSpy);
+
+    t('after from previous group should have been called before', () => {
+      sinon.assert.calledOnce(firstAfterSpy);
+    });
+
+    t.after(secondAfterSpy);
+  });
+
 });
