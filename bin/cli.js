13fe36a9440c983363190b5d1bd26ca6ff2ed893
daniel-lundin <daniel.t.lundin@gmail.com>
2016-12-30 20:47:45 +0100

    Read config from package.json#kuta if present

--- bin/cli.js
+++ bin/cli.js
@@ -4,12 +4,33 @@ const path = require('path');
 const minimist = require('minimist');
 const colors = require('colors');
 const glob = require('glob');
+const fs = require('fs');
 
 const runner = require(path.join(__dirname, '../lib/runner'));
 
 const EXIT_CODE_USAGE = 1;
 const EXIT_CODE_FAILURES = 2;
 
+function readFromConfig() {
+  return new Promise((resolve) => {
+    fs.readFile('./package.json', (err, data) => {
+      if (err) {
+        return resolve({
+          processes: 4,
+          requires: [],
+          files: []
+        });
+      }
+      const config = JSON.parse(data);
+      const kutaConfig = config.kuta || {};
+      return resolve({
+        processes: kutaConfig.processes || 4,
+        requires: kutaConfig.requires || [],
+        files: kutaConfig.files || []
+      });
+    });
+  });
+}
 function log(...args) {
   console.log(...args); // eslint-disable-line
 }
@@ -27,28 +48,37 @@ function printUsage() {
 }
 
 function promiseGlob(globPattern) {
-  return new Promise((resolve) => {
-    glob(globPattern, (err, files) => resolve(files));
+  return new Promise((resolve, reject) => {
+    glob(globPattern, (err, files) => {
+      if (err) return reject(err);
+      return resolve(files);
+    });
   });
 }
 
+readFromConfig()
+  .then((config) => {
+    const args = minimist(process.argv.slice(2));
+    if (args._.length === 0 && config.files.length < 1) {
+      printUsage();
+    }
 
-const args = minimist(process.argv.slice(2));
-if (args._.length < 1) {
-  printUsage();
-}
+    const requires = [].concat(args.require || []).concat([].concat(args.r || []));
+    const processes = parseInt(args.processes || args.p || 4, 10);
+    const files = args._;
 
-const requires = [].concat(args.require || []).concat([].concat(args.r || []));
-const processPool = parseInt(args.processes || args.p || 4, 10);
-
-const files = args._;
-const filePromises = files
-  .map(promiseGlob)
-  .reduce((acc, curr) => acc.concat(curr), []);
-
-Promise.all(filePromises)
-  .then((files) => files.reduce((acc, curr) => acc.concat(curr), []))
-  .then((files) => runner.run(files, requires, processPool))
+    const filePromises = (files.length ? files : config.files)
+      .map(promiseGlob)
+      .reduce((acc, curr) => acc.concat(curr), []);
+    return Promise.all(filePromises)
+      .then((files) => files.reduce((acc, curr) => acc.concat(curr), []))
+      .then((files) => ({
+        requires: requires.length ? requires : config.requires,
+        processes: processes.length ? processes : config.processes,
+        files
+      }));
+  })
+  .then(({ files, requires, processes }) => runner.run(files, requires, processes))
   .then((results) => {
     log('');
     log(colors.bold(`Passed: ${colors.green(results.successes)}`));
