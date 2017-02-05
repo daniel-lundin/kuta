4af00ccbd20136d99dec5a037fdf4afec33b0b33
daniel-lundin <daniel.t.lundin@gmail.com>
2017-02-05 20:18:47 +0100

    Updated mocha codemod to insert imports

--- codemods/mocha-to-kuta.js
+++ codemods/mocha-to-kuta.js
@@ -1,16 +1,23 @@
 const hooks = ['before', 'after', 'beforeEach', 'afterEach'];
 const cakesKeywords = ['Given', 'When', 'And', 'Then'];
 
+function toLower(keyword) {
+  return keyword[0].toLowerCase() + keyword.slice(1);
+}
+
+function arrayToLower(keywords) {
+  return keywords.map(toLower);
+}
+
 export default function transformer(file, api) {
-  const j = api.jscodeshift; 
-  
+  const j = api.jscodeshift;
+
   function scopeLifecycles(fn, identifier) {
-    console.log('fn', fn)
     const res = j(fn)
       .find(j.ExpressionStatement)
       .filter(path => path.parentPath.parentPath.parentPath.value === fn)
       .filter(path => hooks.indexOf(path.value.expression.callee.name) !== -1);
-      
+
     res.forEach((path) => {
       const member = j.memberExpression(
         j.identifier(identifier),
@@ -21,14 +28,14 @@ export default function transformer(file, api) {
     });
     return fn;
   }
-  
+
   function arrowWithIt(fn) {
     return j.arrowFunctionExpression(
       [j.identifier('it')],
       scopeLifecycles(fn, 'it').body
     )
   }
-  
+
   function createShorthandObject(props) {
     const shorthands = props.map((prop) => {
       const shorthandProp = j.property('init', j.identifier(prop), j.identifier(prop))
@@ -37,14 +44,34 @@ export default function transformer(file, api) {
     });
     return j.objectPattern(shorthands);
   }
-  
+
   const root = j(file.source);
-  
+
+  function addKutaImport(root, hasFeature, hasDescribe) {
+    if (!hasFeature && !hasDescribe) {
+      return;
+    }
+    const body = root.get().value.program.body;
+    const importSpecifiers = [];
+    if (hasFeature) {
+      importSpecifiers.push(j.importSpecifier(j.identifier('feature'), j.identifier('feature')));
+    }
+    if (hasDescribe) {
+      importSpecifiers.push(j.importSpecifier(j.identifier('describe'), j.identifier('describe')));
+    }
+
+    const kutaImport = j.importDeclaration(importSpecifiers, j.literal('kuta'));
+    body.unshift(kutaImport);
+  }
+
+  let hasDescribe = false;
+  let hasFeature = false;
   // Transforms describes...
   root
     .find(j.CallExpression)
     .forEach(path => {
       if (path.node.callee.name === 'describe') {
+        hasDescribe = true;
         const callback = path.value.arguments[1];
         const updatedCallback = arrowWithIt(callback);
         const updatedDescribe = j.callExpression(j.identifier('describe'), [
@@ -54,12 +81,13 @@ export default function transformer(file, api) {
         j(path).replaceWith(updatedDescribe);
       }
     });
-  
+
   // Transforms Feature('foo', () => {}) to feature('foo', ({ feature }) => { });
   root
     .find(j.CallExpression)
     .filter((path) => path.node.callee.name === 'Feature')
     .forEach((path) => {
+      hasFeature = true;
       const callback = path.value.arguments[1];
       const featureShorthandProp = j.property('init', j.identifier('feature'), j.identifier('feature'))
       featureShorthandProp.shorthand = true;
@@ -71,9 +99,9 @@ export default function transformer(file, api) {
         j.identifier('feature'),
         [path.node.arguments[0], newArrow]
       );
-      j(path).replaceWith(updatedFeature)       
+      j(path).replaceWith(updatedFeature);
     });
-  
+
   function getBddKeywords(fn) {
     const res = j(fn)
       .find(j.ExpressionStatement)
@@ -86,21 +114,14 @@ export default function transformer(file, api) {
     });
     return Array.from(keywords)
   }
-  
-  function toLower(keyword) {
-    return keyword[0].toLowerCase() + keyword.slice(1);
-  }
-  
-  function arrayToLower(keywords) {
-    return keywords.map(toLower);
-  }
-  
+
+
   function decapitilizeBddKeywords(fn) {
     const res = j(fn)
       .find(j.ExpressionStatement)
       .filter(path => path.parentPath.parentPath.parentPath.value === fn)
       .filter(path => cakesKeywords.indexOf(path.value.expression.callee.name) !== -1)
-    
+
     res.forEach((path) => {
       const name = path.value.expression.callee.name;
       path.value.expression.callee.name = toLower(name);
@@ -123,9 +144,12 @@ export default function transformer(file, api) {
         [path.node.arguments[0], newArrow]
       );
       j(path).replaceWith(updatedScenario);
-    });  
-  
+    });
+
+  // Add imports
+  addKutaImport(root, hasFeature, hasDescribe);
   return root.toSource({
     arrowParensAlways: true
   });
 }
+
