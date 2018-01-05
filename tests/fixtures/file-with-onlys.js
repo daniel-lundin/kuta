93f938b08fd5b9e482a0cb7dab94a692befc55e4
daniel-lundin <daniel.t.lundin@gmail.com>
2018-01-05 16:05:20 +0100

    Fix only scanning regex

--- tests/fixtures/file-with-onlys.js
+++ tests/fixtures/file-with-onlys.js
@@ -1,38 +1,37 @@
-const assert = require('assert');
-const test = require('../../lib/kuta.js').test;
+const assert = require("assert");
+const test = require("../../lib/kuta.js").test;
 
-test('not only', () => {
-  throw new Error('nooooo');
+test("not only", () => {
+  throw new Error("nooooo");
 });
 
-test.only('has only', () => {});
+test.only("has only", () => {});
 
-test.only('also has only', () => {});
+test.only("also has only", () => {});
 
-test.group('a group without only', (t) => {
-  t('this should not run', () => {
-    throw new Error('nooooo');
+test.group("a group without only", t => {
+  t("this should not run", () => {
+    throw new Error("nooooo");
   });
 
-  t.only('however this should run', () => {});
+  t.only("however this should run", () => {});
 });
 
-test.group('outer group', (outerGroup) => {
+test.group("outer group", outerGroup => {
   let beforeRun = false;
 
   outerGroup.before(() => {
     beforeRun = true;
   });
 
-  outerGroup.group('inner group', (t) => {
-
-    t.only('inner group test', () => {
+  outerGroup.group("inner group", t => {
+    t.only("inner group test", () => {
       assert(beforeRun);
     });
   });
 });
 
-test.group.only('group with only', (t) => {
-  t('this should run', () => { });
-  t('this should run as well', () => { });
+test.group.only("group with only", t => {
+  t("this should run", () => {});
+  t("this should run as well", () => {});
 });
