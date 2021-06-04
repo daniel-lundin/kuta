5f3897f2f8c83cc90d09f1fa3d797cc1632783c2
Andreas Pålsson <andreas.palsson@milkywire.com>
2021-06-04 18:03:15 +0200

    Fix results printing twice for same file in single proces runs

--- src/reporters/specHelpers.js
+++ src/reporters/specHelpers.js
@@ -10,10 +10,17 @@ const XMARK = "✗";
 
 const spaces = length => Array.from({ length: length * 2 }).join(" ");
 
-function header(logger, group, level) {
-  const time = group.time ? `(${group.time} ms)` : '';
+function header(logger, group, level, includeTime) {
+  const time = includeTime && group.time !== null && group.time !== undefined ? `(${group.time} ms)` : "";
   const label = `${spaces(level)} ${colors.bold(group.description)} ${time}`;
   decorateTopLevel(logger, label, level);
+}
+
+function footer(logger, group, level) {
+  if (group.time !== null && group.time !== undefined) {
+    const label = `${spaces(level)} ${colors.bold(group.description)} (${group.time} ms)`;
+    logger.log(label);
+  }
 }
 
 function decorateTopLevel(logger, label, level) {
@@ -26,15 +33,15 @@ function isGroupEmpty(group) {
   return groupSummary.errors === 0 && groupSummary.successes === 0;
 }
 
-function printResults(logger, group, level = 0) {
+function printResults(logger, group, level = 0, includeTimeInHeader = true) {
   if (isGroupEmpty(group)) {
     return;
   }
 
-  header(logger, group, level);
+  header(logger, group, level, includeTimeInHeader);
 
   group.results.forEach(result => {
-    const time = result.time || result.time === 0 ? `(${result.time} ms)` : '';
+    const time = result.time || result.time === 0 ? `(${result.time} ms)` : "";
     if (result.result === common.TEST_SUCCESS) {
       logger.log(
         `${spaces(level + 1)} ${colors.green(CHECKMARK)} ${result.description} ${time}`
@@ -47,8 +54,14 @@ function printResults(logger, group, level = 0) {
     }
   });
 
-  group.groups.forEach(group => printResults(logger, group, level + 1));
+  group.groups.forEach(group => printResults(logger, group, level + 1, includeTimeInHeader));
+
+  if (!includeTimeInHeader) {
+    footer(logger, group, level);
+    group.time = null;
+  }
 }
+
 module.exports = {
   printResults
 };
